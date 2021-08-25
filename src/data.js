bc180de4b21b1e8e3b46b9008d30d73a4fc6d43e
Venturing Guy <LVentura335@Gmail.com>
2021-08-25 01:08:32 -0700

    c3 - created object with category as key

--- src/data.js
+++ src/data.js
@@ -2,5 +2,13 @@ import data from './data.json' // imports data.json
 
 const allCategories = data.map(obj => obj.category) // maps category json data into array
 
+// Make an object where each key is a category name
+const categoryObjects = allCategories.reduce((obj, cat) => {
+    obj[cat] = 0
+    return obj
+  }, {}) // !!! Be sure to define the initial value as an Object!
+  // Make an arr array of the keys
+  const categoriesUnique = Object.keys(categoryObjects)
+  
 
 export default data // export the native JS array
