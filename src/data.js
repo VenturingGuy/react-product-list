e14f82ac2603c67cfba390b0fbc22bd726b50fcb
Venturing Guy <LVentura335@Gmail.com>
2021-08-25 01:02:06 -0700

    2nd challenge comments

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 import data from './data.json' // imports data.json
 
-const allCategories = data.map(obj => obj.category)
+const allCategories = data.map(obj => obj.category) // maps category json data into array
 
 
 export default data // export the native JS array
