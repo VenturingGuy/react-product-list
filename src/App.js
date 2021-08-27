332c46934cbd0e6936e903dd720bffb38dd31228
Venturing Guy <LVentura335@Gmail.com>
2021-08-27 15:35:12 -0700

    display categories, added header titles

--- src/App.js
+++ src/App.js
@@ -11,20 +11,16 @@ console.log(namesAndCategories)
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Products</h1>
+      <h2>Product Count: {data.length}</h2>
+      <h2>Cateogry Count: {categoriesUnique.length}</h2>
+        {namesAndCategories.map(obj => {
+          return (
+            <button>
+              {obj.name}
+            </button>
+          )
+        })}
     </div>
   );
 }
