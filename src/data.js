a32e4066321193c13c8255adef68041266f731c1
Venturing Guy <LVentura335@Gmail.com>
2021-08-27 14:47:32 -0700

    challenge 4

--- src/data.js
+++ src/data.js
@@ -9,6 +9,17 @@ const categoryObjects = allCategories.reduce((obj, cat) => {
   }, {}) // !!! Be sure to define the initial value as an Object!
   // Make an arr array of the keys
   const categoriesUnique = Object.keys(categoryObjects)
-  
+
+const categoriesWithCounts = data.reduce((obj, cat) => {
+  if (obj[cat] === "undefined") {
+    obj[cat] = 1
+  }
+  else {
+    obj[cat] += 1
+  }
+  return obj
+}, {})
+
+console.log(data)
 
 export default data // export the native JS array
